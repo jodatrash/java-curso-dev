96a99027314660f3c0014df7d5aa2a420b8353c8
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-02-04 11:48:50 -0600

    Se añade ejemplo de introducción

--- src/basic/c00_helloworld/HelloWorld.java
+++ src/basic/c00_helloworld/HelloWorld.java
@@ -1,5 +1,9 @@
 package basic.c00_helloworld;
 
+/*
+* Clase 01 - Introducción y Hola Mundo (03/02/2026)
+*/
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
--- src/basic/c01_beginner/VariablesAndConstants.java
+++ src/basic/c01_beginner/VariablesAndConstants.java
@@ -1,4 +1,41 @@
 package basic.c01_beginner;
 
+/*
+ * Clase 02 - Variables y constantes (03/02/2026)
+ */
+
 public class VariablesAndConstants {
+    public static void main(String[] args) {
+        // Variables
+
+        // String
+        String name = "Jonathan";
+        System.out.println(name);
+
+        name = "Jodatrash";
+        // int
+        int age = 27;
+        System.out.println(age);
+        System.out.println(name);
+
+        // var
+
+        /*
+         * Inferencia de tipos
+         */
+
+        var email = "Jonathan.test@test.com";
+        System.out.println(email);
+
+        // Constantes
+
+        /*
+         * Buenas prácticas con las constantes
+         * 1.- Siempre en mayúsculas
+         * 2.- Agregar un final para especificar que es una constante.
+         */
+
+        final String EMAIL = "Jonathan.test@test.com";
+        System.out.println(EMAIL);
+    }
 }
