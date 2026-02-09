eda8ea0bbfa6cae454d993f6c6b5e423f4c0671f
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-02-08 20:24:03 -0600

    Refactor para obtener id y edad usando modificadores de acceso

--- src/basic/c08_oop/Classes.java
+++ src/basic/c08_oop/Classes.java
@@ -2,11 +2,21 @@ package basic.c08_oop;
 
 public class Classes {
     public static void main(String[] args) {
-        Person person = new Person("Jonathan", 27);
+        Person person = new Person("Jonathan", 27, "123456789A");
         person.greetings();
 
-        String surname = person.name = "Jodatrash";
-        System.out.println(surname);
+        // No se puede acceder directamente al "id" como atributo o propiedad.
+        // System.out.println(person.id);
+
+        // Pero se tiene la función para acceder y obtener el "id"
+        System.out.println(person.getId());
+
+        // Con esto si se puede acceder y actualizar la edad, ya que se tiene un setter.
+        person.setAge(50);
+        System.out.println(person.getAge()); // Pero se tiene la función para acceder y obtener "age"
+
+        // String surname = person.name = "Jodatrash";
+        // System.out.println(surname);
     }
 
 }
