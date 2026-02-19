367773b52b0e6370f010425657cde32fb19fd6e2
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-02-18 22:05:20 -0600

    Se añaden las clases del ejercicio No. 1

---
+++ src/basic/c08_oop/InheritanceExercises/ex_01/Car.java
@@ -0,0 +1,7 @@
+package basic.c08_oop.InheritanceExercises.ex_01;
+
+public class Car extends Vehicle {
+    public void honk() {
+        System.out.println("¡Beep beep!");
+    }
+}
---
+++ src/basic/c08_oop/InheritanceExercises/ex_01/Vehicle.java
@@ -0,0 +1,12 @@
+package basic.c08_oop.InheritanceExercises.ex_01;
+
+/*
+ * 1. Crea una clase Vehicle con un método move().
+ * Luego crea una subclase Car que herede de Vehicle y agrega el método honk().
+ */
+
+public class Vehicle {
+    public void move() {
+        System.out.println("El coche se mueve");
+    }
+}
