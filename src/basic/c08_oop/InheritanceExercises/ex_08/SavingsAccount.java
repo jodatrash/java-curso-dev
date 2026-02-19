9d1845b13d6eb5b5fc26e6475a1a04999736e28c
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-02-18 23:46:10 -0600

    Se añade package del ejercicio N0. 8

---
+++ src/basic/c08_oop/InheritanceExercises/ex_08/SavingsAccount.java
@@ -0,0 +1,23 @@
+package basic.c08_oop.InheritanceExercises.ex_08;
+
+import basic.c08_oop.InheritanceExercises.Account;
+
+public class SavingsAccount extends Account {
+    private double interestRate;
+
+    public SavingsAccount(double balance, double interestRate) {
+        super(balance);
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public void displayBalance() {
+        super.displayBalance();
+    }
+
+    public void addInterest() {
+        double interest = getBalance() * interestRate;
+        deposit(interest);
+        System.out.println("Interés aplicado    : $" + interest);
+    }
+}
