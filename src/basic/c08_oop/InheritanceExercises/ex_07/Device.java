020a4b33c6598fa2ccf7b3404f043b667af7e15b
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-02-18 23:46:01 -0600

    Se añade package del ejercicio N0. 7

---
+++ src/basic/c08_oop/InheritanceExercises/ex_07/Device.java
@@ -0,0 +1,12 @@
+package basic.c08_oop.InheritanceExercises.ex_07;
+
+/*
+ * 7. Haz una clase Device con un constructor que imprima “Device created”.
+ * Luego crea Phone que herede de Device y en su constructor imprima “Phone ready”.
+ */
+
+public class Device {
+    public Device(){
+        System.out.println("Device created");
+    }
+}
---
+++ src/basic/c08_oop/InheritanceExercises/ex_07/Phone.java
@@ -0,0 +1,8 @@
+package basic.c08_oop.InheritanceExercises.ex_07;
+
+public class Phone extends Device {
+    public Phone() {
+        super();
+        System.out.println("Phone ready");
+    }
+}
