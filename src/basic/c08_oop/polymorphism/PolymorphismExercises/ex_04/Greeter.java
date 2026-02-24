6c9e3276872e4fff6a98426413dc3782725a674f
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-02-23 23:16:19 -0600

    Se añade package ex_04 para ejercicio No.04

---
+++ src/basic/c08_oop/polymorphism/PolymorphismExercises/ex_04/Greeter.java
@@ -0,0 +1,11 @@
+package basic.c08_oop.polymorphism.PolymorphismExercises.ex_04;
+
+public class Greeter {
+    public void greet() {
+        System.out.println("Hello!");
+    }
+
+    public void greet(String name) {
+        System.out.println("Hello! " + name);
+    }
+}
