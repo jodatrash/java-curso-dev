881603a170635aa506daf7f545fb1f43fbac914c
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-02-23 23:15:33 -0600

    Se añade package ex_03 para ejercicio No.03

---
+++ src/basic/c08_oop/polymorphism/PolymorphismExercises/ex_03/Printer.java
@@ -0,0 +1,21 @@
+package basic.c08_oop.polymorphism.PolymorphismExercises.ex_03;
+
+public class Printer {
+    public void display() {
+        System.out.println("Cargando Printer");
+    }
+
+    public int value(int a) {
+        return a;
+    }
+
+    public String value(String text) {
+        return text;
+    }
+
+    public double value(double a) {
+        return a;
+    }
+
+
+}
