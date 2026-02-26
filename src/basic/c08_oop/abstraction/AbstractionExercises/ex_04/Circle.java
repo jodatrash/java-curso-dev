918515352b326cd868f62866928ddaf6bfa643f1
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-02-25 19:40:26 -0600

    Se añaden ejercicios al package ex_04

---
+++ src/basic/c08_oop/abstraction/AbstractionExercises/ex_04/Circle.java
@@ -0,0 +1,8 @@
+package basic.c08_oop.abstraction.AbstractionExercises.ex_04;
+
+public class Circle implements Drawable {
+    @Override
+    public void draw() {
+        System.out.println("Dibujando un circulo...");
+    }
+}
