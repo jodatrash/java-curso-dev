5254d51a6ff68c777a9fc098358de2ed7eeba78e
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-02-25 23:28:22 -0600

    Se añade el package para ejercicio No.01

---
+++ src/basic/c09_exeptions/ex_01/Division.java
@@ -0,0 +1,11 @@
+package basic.c09_exeptions.ex_01;
+
+public class Division {
+
+    int num1 = 25, num2 = 0;
+    int result;
+
+    public int getResult() {
+        return result = num1 * num2;
+    }
+}
