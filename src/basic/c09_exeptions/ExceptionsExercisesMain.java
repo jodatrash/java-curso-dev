cf448f48f63eaecdb91ab0c3a949cb6ab72c1b84
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-02-27 20:24:17 -0600

    Se aladen los ejercicios 5 y 6

--- src/basic/c09_exeptions/ExceptionsExercisesMain.java
+++ src/basic/c09_exeptions/ExceptionsExercisesMain.java
@@ -33,9 +33,18 @@ public class ExceptionsExercisesMain {
         // 4. Escribe una función que transforma texto a número. Usa try-catch para manejar entradas no válidas (NumberFormatException).
 
         // 5. Escribe un programa con un bloque finally que se ejecute siempre, haya o no error.
+        try {
+            var result = 10 / 0;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error");
+        } finally {
+            System.out.println("Acaba el proceso.");
+        }
 
         // 6. Usa throw para lanzar un IllegalArgumentException si un número introducido es negativo.
-
+        var validate = new ValidatorNum();
+        validate.numberValidate(-10);
         // 7. Crea una clase TemperatureChecker que lanza una excepción personalizada si la temperatura es menor a -50 o mayor a 50.
 
         // 8. Crea un programa con varios bloques catch: uno para ArithmeticException, otro para ArrayIndexOutOfBoundsException.
@@ -43,5 +52,14 @@ public class ExceptionsExercisesMain {
         // 9. Crea una función checkPassword(String pass) que lance una excepción si la contraseña es demasiado corta.
 
         // 10. Implementa una clase LoginSystem que use una excepción personalizada LoginFailedException si el usuario o contraseña son incorrectos.
+    }
+
+    public static class ValidatorNum {
+        public void numberValidate(int number) throws IllegalArgumentException {
+            if (number < 0) {
+                throw new IllegalArgumentException("No puedes ingresar números negativos");
+            }
+        }
+
     }
 }
