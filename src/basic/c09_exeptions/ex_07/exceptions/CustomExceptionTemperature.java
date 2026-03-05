d7491b06c84d9eb7f73d86e60a075bcf96e1e35e
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-03-04 19:08:58 -0600

    Se añade package exceptions

---
+++ src/basic/c09_exeptions/ex_07/exceptions/CustomExceptionTemperature.java
@@ -0,0 +1,7 @@
+package basic.c09_exeptions.ex_07.exceptions;
+
+public class CustomExceptionTemperature extends Exception{
+    public CustomExceptionTemperature(String message) {
+        super(message);
+    }
+}
