d58df1d433d917a55e7525b4cc7cd106ba5a79f1
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-03-11 21:20:05 -0600

    Se añade package para la excepciones personalizada

---
+++ src/basic/c09_exeptions/ex_08/exceptions/WeakPasswordException.java
@@ -0,0 +1,7 @@
+package basic.c09_exeptions.ex_08.exceptions;
+
+public class WeakPasswordException extends Exception {
+    public WeakPasswordException(String message) {
+        super(message);
+    }
+}
