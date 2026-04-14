c8531d3f4473884ff12c4a23cfa33c0ca364e9d9
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-04-13 21:33:41 -0600

    Se añade primer ejercicio de curso

---
+++ src/cursoAGF/Main.java
@@ -0,0 +1,11 @@
+package cursoAGF;
+
+public class Main {
+    public static void main(String[] args) {
+        String greetings = "Hola mundo";
+        System.out.println(greetings);
+
+        System.out.println(greetings.toUpperCase()); /*Todo mayúsculas*/
+        System.out.println(greetings.toLowerCase()); /*Todo minúsculas*/
+    }
+}
