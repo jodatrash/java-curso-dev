0c449fff7ef4bf15c4471c058b7de7fb34951891
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-04-13 22:19:18 -0600

    Se añade primitivos.java - ejercicio de curso

---
+++ src/cursoAGF/Primitivos.java
@@ -0,0 +1,12 @@
+package cursoAGF;
+
+public class Primitivos {
+    public static void main(String[] args) {
+        /*Primitivos - Numéricos: Enteros*/
+
+        byte numeroByte = 8;
+
+        String text = "bits";
+        System.out.println("numeroByte = " + numeroByte + text);
+    }
+}
