c7c430c43d14d592ddcd1388026ef7a7c7686c38
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-05-03 22:00:39 -0600

    Se añade ejercicio integrador

---
+++ src/practice/clde/TarjetaPresentacion.java
@@ -0,0 +1,16 @@
+package practice.clde;
+
+public class TarjetaPresentacion {
+    private static final String SEPARADOR = "=".repeat(42);
+    public static void main(String[] args) {
+        System.out.println(SEPARADOR);
+        System.out.print("\tTARJETA DE PRESENTACIÓN\n");
+        System.out.println(SEPARADOR);
+        System.out.println("Nombre: Jonathan Pacheco Cruz");
+        System.out.println("Edad: 27 años");
+        System.out.println("Ciudad: Altamira");
+        System.out.println("Profesión / Ocupación: L3 Support");
+        System.out.println("Pasatiempo favorito: Videojuegos");
+        System.out.println(SEPARADOR);
+    }
+}
